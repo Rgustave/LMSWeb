c974d0ac9f591588f6563adc005c2d44f28a81be
gustavemugabo <mugustave02@gmail.com>
2017-03-02 17:47:11 -0500

    PublisherDAO and BookCopyDAO  updated

--- src/main/java/com/gcit/lms/dao/PublisherDAO.java
+++ src/main/java/com/gcit/lms/dao/PublisherDAO.java
@@ -6,85 +6,71 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.jdbc.core.ResultSetExtractor;
+
 import com.gcit.lms.entity.Book;
+import com.gcit.lms.entity.LibraryBranch;
 import com.gcit.lms.entity.Publisher;
 
-public class PublisherDAO extends BaseDAO {
+public class PublisherDAO extends BaseDAO implements ResultSetExtractor<List<Publisher>>   {
 	
-	public PublisherDAO(Connection conn) {
-		super(conn);
-	}
 	
 	public Publisher readPublisherByPk(Integer publisherId) throws ClassNotFoundException, SQLException{
-		List<Publisher> publishers = readAll("select * from tbl_publisher where publisherId = ?", new Object[]{publisherId});
+		List<Publisher> publishers = template.query("select * from tbl_publisher where publisherId = ?", new Object[]{publisherId},this);
 		if(publishers!=null && !publishers.isEmpty()){
 			return publishers.get(0);
 		}
 		return null;
 	}
 	public List<Publisher> readAllPublishers() throws ClassNotFoundException, SQLException {
-		return readAll("SELECT * FROM tbl_publisher", null);
+		return template.query("SELECT * FROM tbl_publisher", this);
 	}
 	
 	public Integer addPublisherWithID(Publisher publisher) throws ClassNotFoundException, SQLException {
 		
-				return saveWithID("insert into tbl_publisher (publisherName,publisherAddress,publisherPhone) values (? , ?,?)",
+				return template.update("insert into tbl_publisher (publisherName,publisherAddress,publisherPhone) values (? , ?,?)",
 				new Object[] {publisher.getPublisherName(),publisher.getPublisherAddress(),publisher.getPublisherPhone() });
 	}
 	public List<Publisher>readAllPublisher() throws ClassNotFoundException,SQLException{
 		
-		return readAll("SELECT * FROM tbl_publisher",null);
+		return template.query("SELECT * FROM tbl_publisher",this);
 	}
 	public void addGenre(Publisher publisher) throws SQLException, ClassNotFoundException {
-		save("insert into tbl_publisher (publisherName,publisherAddress,publisherPhone)"
+		template.update("insert into tbl_publisher (publisherName,publisherAddress,publisherPhone)"
 				+ " values (?,?,?)", new Object[] {publisher.getPublisherName(),publisher.
 						getPublisherAddress(),publisher.getPublisherPhone()});
 	}
 	public void updatePublisher(Publisher publisher) throws SQLException, ClassNotFoundException {
-		save("update tbl_publisher set publisherName = ? ,publisherAddress =? ,publisherPhone = ?"
+		template.update("update tbl_publisher set publisherName = ? ,publisherAddress =? ,publisherPhone = ?"
 				+ " where publisherId = ?", new Object[] 
 				{publisher.getPublisherName(),publisher.
 					getPublisherAddress(),publisher.getPublisherPhone(),publisher.getPublisherId()});
 	}
 	public void deletePublisher(Publisher publisher) throws SQLException, ClassNotFoundException {
-		save("delete  from tbl_publisher where publisherId = ?", new Object[] {publisher.getPublisherId()});
+		template.update("delete  from tbl_publisher where publisherId = ?", new Object[] {publisher.getPublisherId()});
 	}
 
 	@Override
-	public List<Publisher> extractData(ResultSet rs) throws SQLException, ClassNotFoundException {
+	public List<Publisher> extractData(ResultSet rs) throws SQLException {
 		
 		List<Publisher> publishers = new ArrayList<>();
 		while(rs.next()){
 			Publisher publisher = new Publisher();
-			BookDAO bkdao = new BookDAO(conn);
 			publisher.setPublisherId(rs.getInt("publisherId"));
 			publisher.setPublisherName(rs.getString("publisherName"));
 			publisher.setPublisherAddress(rs.getString("publisherAddress"));
 			publisher.setPublisherPhone(rs.getString("publisherPhone"));
-		    publisher.setBooks(bkdao.readAllFirstLevel("select * from tbl_book where pubId =? ",
-						     new Object[]{publisher.getPublisherId()}));
+		    
 			
 		    publishers.add(publisher);
 		}
 		return publishers;
 	}
 
-	@Override
-	public  List<Publisher> extractDataFirstLevel(ResultSet rs) throws SQLException, ClassNotFoundException {
-		List<Publisher> publishers = new ArrayList<>();
-		while(rs.next()){
-			Publisher publisher = new Publisher();
-			publisher.setPublisherId(rs.getInt("publisherId"));
-			publisher.setPublisherName(rs.getString("publisherName"));
-			publisher.setPublisherAddress(rs.getString("publisherAddress"));
-			publisher.setPublisherPhone(rs.getString("publisherPhone"));
-		    publishers.add(publisher);
-		}
-		return publishers;
-	}
+
 
 	public List<Publisher> readPublisherByName(String publisherName) throws ClassNotFoundException, SQLException {
-		return readAll("select * from tbl_publisher where publisherName like ?", new Object[] { "%" + publisherName + "%" });
+		return template.query("select * from tbl_publisher where publisherName like ?", new Object[] { "%" + publisherName + "%" },this);
 	}
 
 }
