75a886fd8142f06862894303c18fbe553e043853
gustavemugabo <mugustave02@gmail.com>
2017-03-02 17:12:24 -0500

    BorrowerDAO errors fixed;

--- src/main/java/com/gcit/lms/dao/BorrowerDAO.java
+++ src/main/java/com/gcit/lms/dao/BorrowerDAO.java
@@ -6,41 +6,42 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.jdbc.core.ResultSetExtractor;
+
 import com.gcit.lms.entity.Author;
 import com.gcit.lms.entity.BookLoan;
 import com.gcit.lms.entity.Borrower;
+import com.gcit.lms.entity.Genre;
 
-public class BorrowerDAO  extends BaseDAO {
-	public BorrowerDAO(Connection conn) {
-		super(conn);
-	}
+public class BorrowerDAO  extends BaseDAO implements ResultSetExtractor<List<Borrower>> {
+	
 	public Integer addABorrowerDAOWithID(Borrower borrower) throws ClassNotFoundException, SQLException {
-		return saveWithID("INSERT INTO tbl_borrower  (address,name,phone)  VALUES(?,?,?)", 
+		return  template.update("INSERT INTO tbl_borrower  (address,name,phone)  VALUES(?,?,?)", 
 				new Object[] { borrower.getAddress(),borrower.getName(),borrower.getPhone() });
 	}
 	public void addBorrower(Borrower borrower) throws SQLException, ClassNotFoundException {
-		save("INSERT INTO tbl_borrower  (address,name,phone)  VALUES(?,?,?)", 
+		template.update("INSERT INTO tbl_borrower  (address,name,phone)  VALUES(?,?,?)", 
 				new Object[] { borrower.getAddress(),borrower.getName(),borrower.getPhone()});
 	}
 	public void updateBorrower(Borrower borrower) throws SQLException, ClassNotFoundException {
-		save("UPDATE    tbl_borrower  SET address =?,phone = ? ,name = ? WHERE  cardNo=?", 
+		template.update("UPDATE    tbl_borrower  SET address =?,phone = ? ,name = ? WHERE  cardNo=?", 
 				new Object[] {borrower.getAddress(),borrower.getPhone(),
 						borrower.getName(),borrower.getCardNo()});
 	}
 	public void deleteBorrower(Borrower borrower) throws SQLException, ClassNotFoundException {
-		save("delete from tbl_borrower   where cardNo = ?", new Object[] {borrower.getCardNo()});
+		template.update("delete from tbl_borrower   where cardNo = ?", new Object[] {borrower.getCardNo()});
 	}
 	
 	public List<Borrower> readAllBorrowers() throws ClassNotFoundException, SQLException{
-		return readAll("select * from tbl_borrower", null);
+		return template.query("select * from tbl_borrower", this);
 	}
 	
 	public List<Borrower> readAllBorrowerByName(String borrowerName) throws ClassNotFoundException, SQLException{
-		return readAll("select * from tbl_borrower where name like ?", new Object[]{"%"+borrowerName+"%"});
+		return template.query("select * from tbl_borrower where name like ?", new Object[]{"%"+borrowerName+"%"},this);
 	}
 	
 	public Borrower readAuthorByPk(Integer borrowerId) throws ClassNotFoundException, SQLException{
-		List<Borrower> borrowers = readAll("select * from tbl_borrower where cardNo = ?", new Object[]{borrowerId});
+		List<Borrower> borrowers = template.query("select * from tbl_borrower where cardNo = ?", new Object[]{borrowerId},this);
 		if(borrowers!=null && !borrowers.isEmpty()){
 			return borrowers.get(0);
 		}
@@ -51,9 +52,7 @@ public class BorrowerDAO  extends BaseDAO {
 	public  List<Borrower> extractData(ResultSet rs) throws SQLException {
 		List<Borrower> borrowers = new ArrayList<>();
 		while(rs.next()){
-			Borrower b= new Borrower();
-			BookDAO bkdao = new BookDAO(conn);
-			
+			Borrower b= new Borrower();			
 			b.setAddress(rs.getString("address"));
 			b.setName(rs.getString("name"));
 			b.setPhone(rs.getString("phone"));
@@ -64,13 +63,9 @@ public class BorrowerDAO  extends BaseDAO {
 		return borrowers;
 	}
 
-	@Override
-	public List<Borrower> extractDataFirstLevel(ResultSet rs) throws SQLException, ClassNotFoundException {
-	
-		return null;
-	}
+
 	public Borrower readBorrowerByPk(Integer borrowerID) throws ClassNotFoundException, SQLException {
-		List<Borrower> borrowers = readAll("select * from tbl_borrower where cardNo = ?", new Object[] { borrowerID });
+		List<Borrower> borrowers = template.query("select * from tbl_borrower where cardNo = ?", new Object[] { borrowerID},this);
 		if (borrowers != null && !borrowers.isEmpty()) {
 			return borrowers.get(0);
 		}
